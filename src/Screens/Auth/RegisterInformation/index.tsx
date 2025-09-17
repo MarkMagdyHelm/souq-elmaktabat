b696af30f3dc71c091fd68868da71bd0391cd486
albertEmil1993 <albert.emil1993@gmail.com>
2025-09-17 16:49:39 +0300

    update

--- src/Screens/Auth/RegisterInformation/index.tsx
+++ src/Screens/Auth/RegisterInformation/index.tsx
@@ -13,11 +13,13 @@ import useToastNotification from '../../../Components/CustomHooks/useToastNotifi
 import { FormikProps } from 'formik';
 import HeaderWithText from '../../../Components/Headers/HeaderWithText';
 import Section from './Componnent/Section';
-import {  CheckBoxEmptyIconBig, CheckBoxIconBig } from '../../../Assets/Svg';
+import { CheckBoxEmptyIconBig, CheckBoxIconBig } from '../../../Assets/Svg';
 import { GetAllRegionsByCountryIdHandler } from '../../../Apis/Appinfo';
 import { useRoute } from '@react-navigation/native';
 import FormStep1 from './Componnent/FormStep1';
 import FormStep2 from './Componnent/FormStep2';
+import FormStep3 from './Componnent/FormStep3';
+
 import { SignUpHandler } from '../../../Apis/User';
 
 type Props = {
@@ -45,7 +47,8 @@ const Index = (props: Props) => {
         showRols: false,
         loadingSignin: false,
         areas: [],
-        isAgreeOnTerms: false
+        isAgreeOnTerms: false,
+        IsRorleCustommer: true
     });
     const keyboard = useKeyboard();
     const dispatch = useDispatch();
@@ -76,7 +79,9 @@ const Index = (props: Props) => {
     }
     const formikRef1 = useRef<FormikProps<any>>(null);
     const formikRef2 = useRef<FormikProps<any>>(null);
+    const formikRef3 = useRef<FormikProps<any>>(null);
     const handleForms = () => {
+
         formikRef1.current?.handleSubmit();
         formikRef2.current?.handleSubmit();
         const isFormik1IsEmpty = Object.keys(formikRef1?.current?.errors).length === 0;
@@ -92,6 +97,7 @@ const Index = (props: Props) => {
         return true;
     }
     const handleBody = () => {
+        const bodyFormData = new FormData();
         let body = { ...formikRef1?.current?.values };
         body.OtherPhoneNumbers = formikRef2?.current?.values?.OtherPhoneNumbers?.length != 0 ? [formikRef2?.current?.values?.OtherPhoneNumbers] : [];
         body.Activities = formikRef2?.current?.values?.Activities.filter(item => item.id !== undefined) || [];
@@ -101,8 +107,30 @@ const Index = (props: Props) => {
             Region: formikRef2?.current?.values?.Area,
             Street: formikRef2?.current?.values?.Address
         }];
+        body.Description = ""
+        body.ImageUrl2 = ""
+        body.ImageUrl = ""
+        body.CompanyName = ""
         delete body.ConfirmPassword;
-        return body;
+        const appendFormData = (data, parentKey = "") => {
+            if (Array.isArray(data)) {
+                data.forEach((value, index) => {
+                    appendFormData(value, `${parentKey}[${index}]`);
+                });
+            } else if (typeof data === "object" && data !== null) {
+                Object.keys(data).forEach(key => {
+                    appendFormData(data[key], parentKey ? `${parentKey}.${key}` : key);
+                });
+            } else {
+                bodyFormData.append(parentKey, data ?? "");
+            }
+        };
+
+        appendFormData(body);
+        console.log('====================================');
+        console.log(bodyFormData);
+        console.log('====================================');
+        return bodyFormData;
     }
     const handleSubmmit = () => {
         const isFormsValid = handleForms();
@@ -120,7 +148,7 @@ const Index = (props: Props) => {
         } else {
             body = handleBody()
         }
-         console.log('===============final=====================');
+        console.log('===============final=====================');
         console.log(formikRef1?.current?.values, formikRef2?.current?.values);
         console.log('====================================');
         console.log('===============final========body=============');
@@ -135,48 +163,48 @@ const Index = (props: Props) => {
                 console.log('====================================');
             } else {
                 showToast({ type: 'error', message: t("Some Fields has incorrect Values!") });
-                handleBackendErrors(res?.message )
+                handleBackendErrors(res?.message)
             }
             setstate(old => ({ ...old, loading: false }))
         }))
-       
+
     }
 
-  const handleBackendErrors = (message: string) => {
-  const errorsArray = message.split(" , ").map(err => err.trim());
+    const handleBackendErrors = (message: string) => {
+        const errorsArray = message.split(" , ").map(err => err.trim());
 
-  // Collect errors for each form
-  const errors1: Record<string, string> = {};
-  const errors2: Record<string, string> = {};
+        // Collect errors for each form
+        const errors1: Record<string, string> = {};
+        const errors2: Record<string, string> = {};
 
-  errorsArray.forEach(err => {
-    const match = err.match(/The (.+?) field/);
-    if (match) {
-      const fieldName = match[1]; 
-      const key = fieldName.charAt(0) + fieldName.slice(1);
+        errorsArray.forEach(err => {
+            const match = err.match(/The (.+?) field/);
+            if (match) {
+                const fieldName = match[1];
+                const key = fieldName.charAt(0) + fieldName.slice(1);
 
-      if (["Name", "Email", "Password","PhoneNumber"].includes(key)) {
-        errors1[key] = err; 
-      } else if (["Addresses"].includes(key)) {
-        
-        errors2[key] = err; 
-      }
-    }
-  });
-console.log('====================================');
-console.log(errors1,errors2);
-console.log('====================================');
-formikRef1.current?.setErrors(errors1);
-  formikRef2.current?.setErrors(errors2);
-  if (Object.keys(errors1).length > 0) {
-    setActiveStep(1)
-  }
+                if (["Name", "Email", "Password", "PhoneNumber"].includes(key)) {
+                    errors1[key] = err;
+                } else if (["Addresses"].includes(key)) {
 
-  if (Object.keys(errors2).length > 0) {
-    setActiveStep(2)
-  }
- 
-};
+                    errors2[key] = err;
+                }
+            }
+        });
+        console.log('====================================');
+        console.log(errors1, errors2);
+        console.log('====================================');
+        formikRef1.current?.setErrors(errors1);
+        formikRef2.current?.setErrors(errors2);
+        if (Object.keys(errors1).length > 0) {
+            setActiveStep(1)
+        }
+
+        if (Object.keys(errors2).length > 0) {
+            setActiveStep(2)
+        }
+
+    };
 
     return (
         <Container
@@ -188,6 +216,7 @@ formikRef1.current?.setErrors(errors1);
                 noPadding
                 style={styles.body}
             >
+                {/* form1 */}
                 <Section
                     title={t("regtxt1")}
                     step={1}
@@ -201,30 +230,60 @@ formikRef1.current?.setErrors(errors1);
                         setActiveStep={setActiveStep}
                         roles={roles}
                         styles={styles}
+                        onSelectRole={(val) => { setstate(old => ({ ...old, IsRorleCustommer: val })) }}
                     />
                 </Section>
                 <View style={{ paddingVertical: PixelPerfect(8) }} />
-                <Section
-                    title={t("regtxt2")}
-                    step={2}
-                    activeStep={activeStep}
-                    setActiveStep={setActiveStep}
-                    onSubmmit={() => { }}
-                >
-                    <FormStep2
-                        formikRef={formikRef2}
-                        state={state}
-                        setstate={setstate}
-                        GetAreas={GetAreas}
-                        countries={countries}
-                        activites={activites}
-                        styles={styles}
-                    />
-                </Section>
+                {/* form2 */}
+                {state.IsRorleCustommer ?
+                    <>
+                        <Section
+                            title={t("regtxt2")}
+                            step={2}
+                            activeStep={activeStep}
+                            setActiveStep={setActiveStep}
+                            onSubmmit={() => { }}
+                        >
+                            <FormStep2
+                                formikRef={formikRef2}
+                                state={state}
+                                setstate={setstate}
+                                GetAreas={GetAreas}
+                                countries={countries}
+                                activites={activites}
+                                styles={styles}
+                            />
+                        </Section>
+                        <View style={{ paddingVertical: PixelPerfect(8) }} />
+                    </>
+                    :
+                    <>
+                        {/* form3 */}
+                        <Section
+                            title={t("regtxt3")}
+                            step={2}
+                            activeStep={activeStep}
+                            setActiveStep={setActiveStep}
+                            onSubmmit={() => { }}
+                        >
+                            <FormStep3
+                                formikRef={formikRef3}
+                                state={state}
+                                setstate={setstate}
+                                GetAreas={GetAreas}
+                                countries={countries}
+                                activites={activites}
+                                styles={styles}
+                            />
+                        </Section>
+                    </>
+                }
+
+
+
                 <Pressable style={[layout.rowBox, styles.checkcon]} onPress={handleagreeonterms}>
                     {state.isAgreeOnTerms ? <CheckBoxIconBig /> : <CheckBoxEmptyIconBig />}
                     <Text style={styles.agreeText}
-
                     >{t("Agree on")}
                         <Text style={[styles.agreeText1]} onPress={() => console.log("gagfagafgafga")}> {t("Privcy and terms")}</Text>
                     </Text>
